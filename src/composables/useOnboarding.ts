864e08aebdb5698288782c9da315301dc2e23dfa
Daniil Rudnov <15265510+rudnovd@users.noreply.github.com>
2026-08-15 18:38:13 +0500

    fix(useOnboarding): change last step if daily route completed

--- src/composables/useOnboarding.ts
+++ src/composables/useOnboarding.ts
@@ -1,10 +1,11 @@
-import type { Driver } from 'driver.js'
+import type { Config, Driver } from 'driver.js'
 import { createSharedComposable } from '@vueuse/core'
 import { driver } from 'driver.js'
 import { customRef, nextTick } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRouter } from 'vue-router'
 import { IS_ONBOARDING_FINISHED_KEY, ONBOARDING_STEP_KEY } from '@/constants/onboarding'
+import { useRouteStore } from '@/stores/route'
 import { useUserStore } from '@/stores/user'
 import 'driver.js/dist/driver.css'
 import '@/assets/styles/onboarding.css'
@@ -13,6 +14,7 @@ export const useOnboarding = createSharedComposable(() => {
   const { t } = useI18n()
   const router = useRouter()
   const userStore = useUserStore()
+  const routeStore = useRouteStore()
   const onboardingStep = customRef<number | null, number>((track, trigger) => {
     return {
       get() {
@@ -26,89 +28,92 @@ export const useOnboarding = createSharedComposable(() => {
       },
     }
   })
-  const instance: Driver = driver({
-    nextBtnText: t('onboarding.buttons.next'),
-    prevBtnText: t('onboarding.buttons.prev'),
-    doneBtnText: t('onboarding.buttons.finish'),
-    popoverClass: 'onboarding-popover',
-    overlayClickBehavior: undefined,
-    allowClose: true,
-    waitForElement: 10_000,
-    stagePadding: 0,
-    steps: [
-      {
-        element: '[data-onboarding-element="map-container"]',
-        popover: {
-          title: t('onboarding.steps[0].title'),
-          description: t('onboarding.steps[0].description'),
-          side: 'bottom',
-          align: 'center',
+  function getConfig(): Config {
+    return {
+      nextBtnText: t('onboarding.buttons.next'),
+      prevBtnText: t('onboarding.buttons.prev'),
+      doneBtnText: t('onboarding.buttons.finish'),
+      popoverClass: 'onboarding-popover',
+      overlayClickBehavior: undefined,
+      allowClose: true,
+      waitForElement: 10_000,
+      stagePadding: 0,
+      steps: [
+        {
+          element: '[data-onboarding-element="map-container"]',
+          popover: {
+            title: t('onboarding.steps[0].title'),
+            description: t('onboarding.steps[0].description'),
+            side: 'bottom',
+            align: 'center',
+          },
+          data: {
+            path: '/settings/edit?target=start',
+          },
         },
-        data: {
-          path: '/settings/edit?target=start',
+        {
+          element: '[data-onboarding-element="save-target-button"]',
+          popover: {
+            description: t('onboarding.steps[1].description'),
+            side: 'bottom',
+            align: 'center',
+            disableButtons: ['next'],
+          },
+          advanceOnClick: true,
+          data: {
+            path: '/settings/edit?target=start',
+          },
         },
+        {
+          element: '[data-onboarding-element="map-container"]',
+          popover: {
+            title: t('onboarding.steps[2].title'),
+            description: t('onboarding.steps[2].description'),
+            side: 'bottom',
+            align: 'center',
+          },
+          data: {
+            path: '/settings/edit?target=radius',
+          },
+        },
+        {
+          element: '[data-onboarding-element="save-target-button"]',
+          popover: {
+            description: t('onboarding.steps[3].description'),
+            side: 'bottom',
+            align: 'center',
+            disableButtons: ['next'],
+          },
+          advanceOnClick: true,
+          data: {
+            path: '/settings/edit?target=radius',
+          },
+        },
+        {
+          element: routeStore.isDailyRouteCompleted ? '.index-page' : '[data-onboarding-element="start-daily-route-button"]',
+          popover: {
+            description: t('onboarding.steps[4].description'),
+            side: 'bottom',
+            align: 'center',
+          },
+          disableActiveInteraction: true,
+          data: {
+            path: '/',
+          },
+        },
+      ],
+      onPrevClick: back,
+      onNextClick: next,
+      onDestroyed: () => {
+        localStorage.removeItem(ONBOARDING_STEP_KEY)
+        localStorage.setItem(IS_ONBOARDING_FINISHED_KEY, 'true')
+        if (!userStore.isAuthenticated) {
+          router.push('/profile/signin')
+        }
       },
-      {
-        element: '[data-onboarding-element="save-target-button"]',
-        popover: {
-          description: t('onboarding.steps[1].description'),
-          side: 'bottom',
-          align: 'center',
-          disableButtons: ['next'],
-        },
-        advanceOnClick: true,
-        data: {
-          path: '/settings/edit?target=start',
-        },
-      },
-      {
-        element: '[data-onboarding-element="map-container"]',
-        popover: {
-          title: t('onboarding.steps[2].title'),
-          description: t('onboarding.steps[2].description'),
-          side: 'bottom',
-          align: 'center',
-        },
-        data: {
-          path: '/settings/edit?target=radius',
-        },
-      },
-      {
-        element: '[data-onboarding-element="save-target-button"]',
-        popover: {
-          description: t('onboarding.steps[3].description'),
-          side: 'bottom',
-          align: 'center',
-          disableButtons: ['next'],
-        },
-        advanceOnClick: true,
-        data: {
-          path: '/settings/edit?target=radius',
-        },
-      },
-      {
-        element: '[data-onboarding-element="start-daily-route-button"]',
-        popover: {
-          description: t('onboarding.steps[4].description'),
-          side: 'bottom',
-          align: 'center',
-        },
-        disableActiveInteraction: true,
-        data: {
-          path: '/',
-        },
-      },
-    ],
-    onPrevClick: back,
-    onNextClick: next,
-    onDestroyed: () => {
-      localStorage.removeItem(ONBOARDING_STEP_KEY)
-      localStorage.setItem(IS_ONBOARDING_FINISHED_KEY, 'true')
-      if (!userStore.isAuthenticated) {
-        router.push('/profile/signin')
-      }
-    },
-  })
+    }
+  }
+  const instance: Driver = driver(getConfig())
   async function back() {
     if (onboardingStep.value === null) {
       onboardingStep.value = 1
@@ -134,6 +139,7 @@ export const useOnboarding = createSharedComposable(() => {
     instance.moveNext()
   }
   async function start() {
+    instance.setConfig(getConfig())
     localStorage.setItem(IS_ONBOARDING_FINISHED_KEY, 'false')
     if (!onboardingStep.value) {
       onboardingStep.value = 0
